da6f5118adcce35c36a62ad18333c0d490c53f71
Mohamed Atta <mohamed.ah.atta@gmail.com>
2023-12-07 04:49:21 +0200

    Add my name

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <div className="app-header">
       <img src={logo} className="app-logo" alt="nasdaq logo" />
+      <h1 className="app-dev-name">Mohamed Atta</h1>
     </div>
   );
 };
